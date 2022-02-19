08f8e41bb459a437f7648a52a38890ad137111ca
Christopher Torres <kiko898@hotmail.com>
2022-02-18 20:53:16 -0700

    Logger

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,126 +1,186 @@
 import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-class main {
+public class main extends SimpleFileVisitor<Path> {
+    public static final Logger LOG = Logger.getLogger(main.class.getName());
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Error! directorio no encontrado");
-            System.exit(1);
-        }
-        String direc=args[0];
-        System.out.println("Directorio: " + direc);
-        System.out.println("");
+    private boolean showWords;
+    private Set<String> termSet;
+    private Set<String> stopWords = null;
 
-        //Listemos todas las carpetas y archivos de la carpeta actual
+    public main(boolean showWords) {
+        this.showWords = showWords;
 
-        File carpeta = new File(direc);
-        String[] listado = carpeta.list();
-        if(carpeta.exists()){
-            if (listado == null || listado.length == 0) {
-                System.out.println("No hay elementos dentro de la carpeta actual");
-                return;
-            }
-            else {
-                //Burbuja
-                String aux;
-                for(int i=1; i<=listado.length; i++) {
-                    for(int j=0; j<listado.length-i; j++) {
-                        if( listado[j].compareTo( listado[j+1] ) > 0 ) {
-                            aux   = listado[j];
-                            listado[j]  = listado[j+1];
-                            listado[j+1]= aux;
-                        }
-                    }
-                }
-                for (int i = 0; i <listado.length; i++) {
-                    System.out.println("Archivo a leer: "+listado[i]);
-                    FileReader fi=null;
-                    try {
-                        fi = new FileReader(direc+listado[i]);
+        termSet = new TreeSet<>();
 
-                    } catch (FileNotFoundException ex) {
-                        System.out.println(ex.getMessage());
-                        System.exit(-1);
-                    }
+        stopWords = this.loadStopWords();
 
-                    BufferedReader inputFile = new BufferedReader(fi);
-
-                    String textLine = null;
-
-                    int lineCount = 0;
-                    int wordCount = 0;
-                    int numberCount = 0;
-
-                    String delimiters = "\\s+|,\\s*|\\.\\s*|\\;\\s*|\\:\\s*|\\!\\s*|\\¡\\s*|\\¿\\s*|\\?\\s*|\\-\\s*"
-                            + "|\\[\\s*|\\]\\s*|\\(\\s*|\\)\\s*|\\\"\\s*|\\_\\s*|\\%\\s*|\\+\\s*|\\/\\s*|\\#\\s*|\\$\\s*";
-
-
-                    // Lista con todas las palabras diferentes
-                    ArrayList<String> list = new ArrayList<String>();
-
-                    // Tiempo inicial
-                    long startTime = System.currentTimeMillis();
-                    try {
-                        while ((textLine = inputFile.readLine()) != null) {
-                            lineCount++;
-
-                            if (textLine.trim().length() == 0) {
-                                continue; // la linea esta vacia, continuar
-                            }
-
-                            // separar las palabras en cada linea
-                            String words[] = textLine.split(delimiters);
-
-                            wordCount += words.length;
-
-                            for (String theWord : words) {
-
-                                theWord = theWord.toLowerCase().trim();
-
-                                boolean isNumeric = true;
-
-                                // verificar si el token es un numero
-                                try {
-                                    Double num = Double.parseDouble(theWord);
-                                } catch (NumberFormatException e) {
-                                    isNumeric = false;
-                                }
-
-                                // Si el token es un numero, pasar al siguiente
-                                if (isNumeric) {
-                                    numberCount++;
-                                    continue;
-                                }
-
-                                // si la palabra no esta en la lista, agregar a la lista
-                                if (!list.contains(theWord)) {
-                                    list.add(theWord);
-                                }
-                            }
-                        }
-                        // Obtener tiempo de ejecución
-                        long tiempoEjecucion = System.currentTimeMillis() - startTime;
-                        inputFile.close();
-                        fi.close();
-
-                        System.out.printf("%2.3f  segundos, %2d lineas y %3d palabras\n",
-                                tiempoEjecucion / 1000.00, lineCount, wordCount - numberCount);
-
-                        // Mostrar total de palabras diferentes
-                        System.out.printf("%5d palabras diferentes\n", list.size());
-                        System.out.println("");
-                    } catch (IOException ex) {
-                        System.out.println(ex.getMessage());
-                    }
-                }
-            }
-        }else {
-            System.out.println("No existe la carpeta");
+        try {
+            Handler fileHandler = new FileHandler("results.log", 2000000, 5);
+            fileHandler.setLevel(Level.INFO);
+            fileHandler.setFormatter(new CvsFormatter());
+            LOG.addHandler(fileHandler);
+        } catch (IOException e) {
+            LOG.severe(e.getMessage());
         }
 
+    }
 
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.exit(2);
+        }
+
+        boolean showWords = false;
+
+        if (args.length == 2) {
+            if (args[1].toLowerCase().equals("-v")) {
+                showWords = true;
+            }
+        }
+        //iniciar en este directorio
+        Path startingDir = Paths.get(args[0]);
+
+        //Clase para procesar los archivos
+
+        main contadorLineas = new main(showWords);
+
+        //Iniciar el recorrido de los archivos
+
+        Files.walkFileTree(startingDir, contadorLineas);
 
 
     }
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+        String name = file.toAbsolutePath().toString();
+
+        if(name.toLowerCase().endsWith(".txt")){
+            FileReader fl= null;
+            BufferedReader in = null;
+
+            fl=new FileReader(name);
+            in= new BufferedReader(fl);
+
+            int lineCount = 0;
+            int wordCount=0;
+            int numberCount=0;
+
+            final String delimiters = "\\s+|,\\s*|\\.\\s*|\\;\\s*|\\:\\s*|\\!\\s*|\\¡\\s*|\\¿\\s*|\\?\\s*|\\-\\s*"
+                    + "|\\[\\s*|\\]\\s*|\\(\\s*|\\)\\s*|\\\"\\s*|\\_\\s*|\\%\\s*|\\+\\s*|\\/\\s*|\\#\\s*|\\$\\s*";
+
+            //Lista con todas las palabras diferentes
+            Set<String> wordsSet = new TreeSet<>();
+            String textLine=null;
+
+            //Tiempo inicial
+            long starTime= System.currentTimeMillis();
+
+            while((textLine= in.readLine()) != null){
+                lineCount++;
+                if(textLine.trim().length()==0){
+                    continue; //la linea esta vacia,continuar
+                }
+
+                String words[] = textLine.split(delimiters);
+
+                wordCount += words.length;
+                for (String theWord : words) {
+
+                    theWord = theWord.toLowerCase().trim();
+
+                    boolean isNumeric = true;
+                    String term = theWord.trim().toLowerCase();
+
+                    // verificar si el token es un numero
+                    try {
+                        Double num = Double.parseDouble(theWord);
+                    } catch (NumberFormatException e) {
+                        isNumeric = false;
+                    }
+
+                    // Si el token es un numero, pasar al siguiente
+                    if (isNumeric) {
+                        numberCount++;
+                        continue;
+                    }
+
+                    // si la palabra no esta en la lista, agregar a la lista
+                    if (!stopWords.contains(term)) {
+                        wordsSet.add(term);
+                    }
+                    //Filtrar palabras vacias
+
+                    //if(!stopWords.contains(theWord)){
+                    //   listaSinStopsWords.add(theWord);
+                    //}
+
+                }
+
+
+            }
+            long tiempoEjecucion = System.currentTimeMillis() - starTime;
+            in.close();
+
+            System.out.printf("%s %2.3f segundos %n\t%,d lineas y %,d palabras ", name,
+                    (tiempoEjecucion / 1000.00), lineCount,wordCount-numberCount);
+
+            //Mostrar total de palabras diferentes
+
+            System.out.printf("%,d diferentes %4.2f\n", wordsSet.size(),
+                    (double) wordsSet.size() / (wordCount -numberCount));
+            System.out.println(wordsSet);
+
+            /*String record = String.format("%s,%6.3f,%d,%d",name,(double)(tiempoEjecucion/1000.00),
+                    wordCount - numberCount, wordsSet.size() );*/
+            String nombre= file.getFileName().toString();
+
+            String record= String.format("Nombre %s, Palabras %d, Palabras diferentes %d",nombre,wordCount -numberCount,wordsSet.size());
+
+            LOG.info(record);
+            LOG.info(String.valueOf(wordsSet));
+            if(showWords){
+                displayWords(wordsSet);
+            }
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    private void displayWords(Set<String> words){
+        for (String theWord : words){
+            System.out.printf("%s ", theWord);
+        }
+        System.out.println();
+    }
+
+    private Set<String> loadStopWords() {
+        TreeSet<String> set = new TreeSet<>();
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("stop-word-list.txt"));
+            String word = null;
+
+            while ((word = in.readLine()) != null) {
+                set.add(word.trim());
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return set;
+    }
+
 }
+
